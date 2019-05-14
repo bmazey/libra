64467b7038c668ac58a5943ec19a380c8fd46245
Brandon <b.mazey@nyu.edu>
2019-05-14 16:18:41 -0400

    dockerfile - ports are wrong

--- src/main/java/com/libra/LibraGatewayApplication.java
+++ src/main/java/com/libra/LibraGatewayApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraEntranceApplication {
-
+public class LibraGatewayApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LibraEntranceApplication.class, args);
+		SpringApplication.run(LibraGatewayApplication.class, args);
 	}
-
 }
